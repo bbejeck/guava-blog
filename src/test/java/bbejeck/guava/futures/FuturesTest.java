8673f59885f67d5f3f2d9ba1e7eceb21a8b77993
Bill Bejeck <bbejeck@gmail.com>
2011-11-23 23:43:00 -0500

    Added tests for testing Futures, ListenableFutures

--- src/test/java/bbejeck/guava/futures/FuturesTest.java
+++ src/test/java/bbejeck/guava/futures/FuturesTest.java
@@ -1,26 +1,21 @@
 package bbejeck.guava.futures;
 
-import bbejeck.support.dataservice.SampleDataService;
-import bbejeck.support.lucene.SampleLuceneIndexBuilder;
-import bbejeck.support.lucene.SampleLuceneSearcher;
+import bbejeck.support.model.Person;
 import com.google.common.base.Function;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
-import org.apache.lucene.store.RAMDirectory;
-import org.h2.tools.Server;
-import org.junit.*;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,43 +24,24 @@ import static org.junit.Assert.assertThat;
  * Time: 2:11 PM
  */
 
-public class FuturesTest {
+public class FuturesTest extends FuturesTestBase {
 
-    private static final String dbUrl = "jdbc:h2:mem:test";
-    private static final String insertSql = " CREATE TABLE PERSON(FIRST_NAME VARCHAR(255), LAST_NAME VARCHAR(255)," +
-            " ADDRESS VARCHAR(255), EMAIL VARCHAR(255),ID INT PRIMARY KEY) AS SELECT * FROM CSVREAD('src/main/resources/names.csv')";
-
-    private static Connection connection;
-    private static Statement statement;
-    private static Server dbServer;
-    private static SampleLuceneIndexBuilder luceneIndexBuilder;
-    private static SampleLuceneSearcher luceneSearcher;
-    private static SampleDataService dataService;
-
-    @BeforeClass
-    public static void setUpBeforeAllTests() throws Exception {
-        startH2();
-        setUpIndex();
-        connectToDb();
-        populateDb();
-    }
-
+    private int numberTasks;
+    private CountDownLatch startSignal;
+    private CountDownLatch doneSignal;
+    private ListeningExecutorService testExecutorService;
 
     @Before
     public void setUp() throws Exception {
-        dataService = new SampleDataService(statement);
+        numberTasks = 5;
+        startSignal = new CountDownLatch(1);
+        doneSignal = new CountDownLatch(numberTasks);
+        testExecutorService = MoreExecutors.listeningDecorator(Executors.newCachedThreadPool());
     }
 
     @After
     public void tearDown() {
-        dataService.shutDown();
-    }
-
-    @AfterClass
-    public static void shutDownAfterAllTests() throws Exception {
-        stopH2();
-        closeDbConnection();
-        luceneSearcher.shutDown();
+        testExecutorService.shutdownNow();
     }
 
     @Test
@@ -74,47 +50,109 @@ public class FuturesTest {
         Function<List<String>, ListenableFuture<List<Map<String, String>>>> queryFunction = new Function<List<String>, ListenableFuture<List<Map<String, String>>>>() {
             @Override
             public ListenableFuture<List<Map<String, String>>> apply(final List<String> ids) {
-                return dataService.retrieveDataByRawIdAsync(ids);
+                return dataService.getPersonDataByIdAsync(ids);
             }
         };
 
         ListenableFuture<List<String>> indexSearch = luceneSearcher.searchAsync("(firstName:martin AND lastName:hess)");
 
         ListenableFuture<List<Map<String, String>>> results = Futures.chain(indexSearch, queryFunction);
-        List<Map<String, String>> persons = results.get(5, TimeUnit.SECONDS);
+        List<Map<String, String>> persons = results.get(1, TimeUnit.SECONDS);
         assertThat(persons.size(), is(1));
         Map<String, String> person = persons.get(0);
         assertThat(person.get("first_name"), is("Martin"));
         assertThat(person.get("last_name"), is("Hess"));
     }
 
+    @Test
+    public void testTransformSearch() throws Exception {
 
-    private static void populateDb() throws SQLException {
-        statement = connection.createStatement();
-        statement.execute(insertSql);
+        Function<List<String>, List<Person>> transformSearchResults = new Function<List<String>, List<Person>>() {
+            @Override
+            public List<Person> apply(List<String> ids) {
+                return dataService.getPersonsById(ids);
+            }
+        };
+
+        ListenableFuture<List<String>> indexSearch = luceneSearcher.searchAsync("firstName:martin");
+        ListenableFuture<List<Person>> transformedResults = Futures.transform(indexSearch, transformSearchResults);
+
+        List<Person> persons = transformedResults.get(1, TimeUnit.SECONDS);
+        int expectedSize = 74;
+        assertThat(persons.size(), is(expectedSize));
+        for (Person person : persons) {
+            assertThat(person.firstName, is("Martin"));
+        }
     }
 
-    private static void setUpIndex() throws IOException {
-        luceneIndexBuilder = new SampleLuceneIndexBuilder();
-        RAMDirectory ramDirectory = luceneIndexBuilder.buildIndex();
-        luceneSearcher = new SampleLuceneSearcher(ramDirectory);
+    @Test
+    public void allAsListSuccess() throws Exception {
+        ListenableFuture<List<Person>> lf1 = getFuture("martin", false);
+        ListenableFuture<List<Person>> lf2 = getFuture("bob", false);
+        ListenableFuture<List<Person>> lf3 = getFuture("emily", false);
+        ListenableFuture<List<Person>> lf4 = getFuture("mona", false);
+        ListenableFuture<List<Person>> lf5 = getFuture("tom", false);
+
+        ListenableFuture<List<List<Person>>> lfResults = Futures.allAsList(lf1, lf2, lf3, lf4, lf5);
+        startSignal.countDown();
+        List<List<Person>> listOfPersonLists = lfResults.get();
+        assertThat(listOfPersonLists.size()>0,is(true));
+        for(List<Person> personList : listOfPersonLists){
+            assertThat(personList.size() > 0,is(true));
+        }
     }
 
-    private static void startH2() throws Exception {
-        dbServer = Server.createTcpServer();
-        dbServer.start();
+    @Test(expected = ExecutionException.class)
+    public void allAsListSuccessOneFailure() throws Exception {
+        ListenableFuture<List<Person>> lf1 = getFuture("martin", false);
+        ListenableFuture<List<Person>> lf2 = getFuture("bob", false);
+        ListenableFuture<List<Person>> lf3 = getFuture("emily", true);
+        ListenableFuture<List<Person>> lf4 = getFuture("mona", false);
+        ListenableFuture<List<Person>> lf5 = getFuture("tom", false);
+
+        ListenableFuture<List<List<Person>>> lfResults = Futures.allAsList(lf1, lf2, lf3, lf4, lf5);
+        startSignal.countDown();
+        List<List<Person>> listOfPersonLists = lfResults.get();
+        fail("should not get here");
     }
 
-    private static void stopH2() throws Exception {
-        dbServer.stop();
+    @Test
+    public void successfulAsListSuccessOneFailure() throws Exception {
+        ListenableFuture<List<Person>> lf1 = getFuture("martin", true);
+        ListenableFuture<List<Person>> lf2 = getFuture("bob", false);
+        ListenableFuture<List<Person>> lf3 = getFuture("emily", true);
+        ListenableFuture<List<Person>> lf4 = getFuture("mona", false);
+        ListenableFuture<List<Person>> lf5 = getFuture("tom", false);
+
+        ListenableFuture<List<List<Person>>> lfResults = Futures.successfulAsList(lf1, lf2, lf3, lf4, lf5);
+        startSignal.countDown();
+        List<List<Person>> listOfPersonLists = lfResults.get();
+
+        assertThat(listOfPersonLists.size()==5,is(true));
+
+        //have null values failed
+        assertThat(listOfPersonLists.get(0),is(nullValue()));
+        assertThat(listOfPersonLists.get(2),is(nullValue()));
+
+        //succeeded returned valid results
+        assertThat(listOfPersonLists.get(1).size() > 0,is(true));
+        assertThat(listOfPersonLists.get(3).size() > 0,is(true));
+        assertThat(listOfPersonLists.get(4).size() > 0,is(true));
     }
 
-    private static void connectToDb() throws Exception {
-        Class.forName("org.h2.Driver");
-        connection = DriverManager.getConnection(dbUrl);
-    }
 
-    private static void closeDbConnection() throws Exception {
-        connection.close();
+    private ListenableFuture<List<Person>> getFuture(final String firstName, final boolean error) {
+        return testExecutorService.submit(new Callable<List<Person>>() {
+            @Override
+            public List<Person> call() throws Exception {
+                startSignal.await();
+                if (error) {
+                    throw new RuntimeException("Ooops!");
+                }
+                List<String> ids = luceneSearcher.search("firstName:" + firstName);
+                List<Person> persons = dataService.getPersonsById(ids);
+                return persons;
+            }
+        });
     }
 }
